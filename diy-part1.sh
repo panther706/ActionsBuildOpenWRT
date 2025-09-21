05ba94aef784edd26f5ac44289d35451665fda17
panther706 <43943311+panther706@users.noreply.github.com>
2025-09-21 14:06:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,13 +4,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
-#sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
-
-# homeproxy
-#git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
-
-# luci-app-netwizard
-#git clone https://github.com/sirpdboy/luci-app-netwizard.git package/luci-app-netwizard
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+echo 'src-git qmodem https://github.com/FUjr/modem_feeds.git' >>feeds.conf.default

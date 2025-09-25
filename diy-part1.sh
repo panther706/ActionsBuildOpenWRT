823e4d900f529e6211fc9645ea45b02edaddccca
panther706 <43943311+panther706@users.noreply.github.com>
2025-09-25 15:47:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,6 +6,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #echo 'src-git qmodem https://github.com/FUjr/modem_feeds.git' >>feeds.conf.default
-git clone https://github.com/lq-wq/luci-app-quickstart.git package/luci-app-quickstart
-git clone https://github.com/kiddin9/luci-app-adguardhome.git package/luci-app-adguardhome
 echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
+git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+git clone https://github.com/sirpdboy/luci-app-netspeedtest package/luci-app-netspeedtest
+git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/sirpdboy/luci-app-parentcontrol.git package/luci-app-parentcontrol
+git clone https://github.com/sirpdboy/luci-app-netwizard package/luci-app-netwizard

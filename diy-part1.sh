333416189e5eb3acae02dbbfd63409ff92efb82e
panther706 <43943311+panther706@users.noreply.github.com>
2025-09-28 13:25:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,8 +7,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #echo 'src-git qmodem https://github.com/FUjr/modem_feeds.git' >>feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
-git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
-git clone https://github.com/sirpdboy/luci-app-netspeedtest package/luci-app-netspeedtest
-git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/sirpdboy/luci-app-parentcontrol.git package/luci-app-parentcontrol
-git clone https://github.com/sirpdboy/luci-app-netwizard package/luci-app-netwizard
+#git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+#git clone https://github.com/sirpdboy/luci-app-netspeedtest package/luci-app-netspeedtest
+#git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/luci-app-adguardhome
+#git clone https://github.com/sirpdboy/luci-app-parentcontrol.git package/luci-app-parentcontrol
+#git clone https://github.com/sirpdboy/luci-app-netwizard package/luci-app-netwizard

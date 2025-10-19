c32e11b76981d2d18798012cfb4f5b4051b2c0db
panther706 <43943311+panther706@users.noreply.github.com>
2025-10-19 21:22:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,6 +7,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 git clone https://github.com/linkease/istore.git package/istore
 #echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
-#echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
 
 

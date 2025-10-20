98d4bf37f6390eb9402d8f2c802b01d74d021cd7
panther706 <43943311+panther706@users.noreply.github.com>
2025-10-20 11:13:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,8 +5,9 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
-git clone https://github.com/linkease/istore.git package/istore
 #echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
 #echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+git clone https://github.com/linkease/istore.git package/istore
+git clone https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
 
 

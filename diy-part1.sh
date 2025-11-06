45adfdf2b26902bd63419b59aba3071fae25a8de
panther706 <43943311+panther706@users.noreply.github.com>
2025-11-06 21:21:42 +0800

    Update and rename padavanonly-part1.sh to diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,7 +7,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 #git clone https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
 #git clone https://github.com/FUjr/QModem.git package/qmodem
-git clone https://github.com/linkease/istore.git package/istore
+#git clone https://github.com/linkease/istore.git package/istore
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git package/adguardhome
 
 

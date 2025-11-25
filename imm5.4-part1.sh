26abd0b0fc940036822b57471716a1aa77306348
panther706 <43943311+panther706@users.noreply.github.com>
2025-11-25 18:40:52 +0800

    Update imm5.4-part1.sh

--- imm5.4-part1.sh
+++ imm5.4-part1.sh
@@ -7,4 +7,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 #git clone https://github.com/linkease/istore.git package/istore
 git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
-git clone https://github.com/Siriling/5G-Modem-Support.git package/adguardhome
+git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support

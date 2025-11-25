5db5e53c72b0e6db279c4c9db7973017dcc0fb2f
panther706 <43943311+panther706@users.noreply.github.com>
2025-11-25 20:59:16 +0800

    Clone 5G-Modem-Support and remove old directory

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,6 +7,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 #git clone https://github.com/linkease/istore.git package/istore
 git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
-#git clone https://github.com/Siriling/5G-Modem-Support.git package/adguardhome
+rm -rf package/mtk/applications/5g-modem
+git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
 
 

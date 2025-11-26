e5b88b6ef5e486910b7ea0bf5e9bb33fddf3d0c5
panther706 <43943311+panther706@users.noreply.github.com>
2025-11-26 18:18:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,7 +7,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 #git clone https://github.com/linkease/istore.git package/istore
 git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
-rm -rf package/mtk/applications/5g-modem
-git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
+#rm -rf package/mtk/applications/5g-modem
+#git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
 
 

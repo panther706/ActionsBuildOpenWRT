3a43e7597b2726759c5ff90867fd7539fae9f203
panther706 <43943311+panther706@users.noreply.github.com>
2025-12-05 14:58:45 +0800

    Update openwrt-part1.sh

--- openwrt-part1.sh
+++ openwrt-part1.sh
@@ -6,6 +6,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 #git clone https://github.com/kenzok78/luci-app-adguardhome.git package/adguardhome
-#git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git package/adguardhome
-git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
+git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git package/adguardhome
+#git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
 git clone https://github.com/FUjr/QModem.git package/QModem

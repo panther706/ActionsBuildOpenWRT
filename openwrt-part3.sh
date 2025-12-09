a701a8d1b7d2fd9ca43888a8a6817353e800cd27
panther706 <43943311+panther706@users.noreply.github.com>
2025-12-09 18:55:05 +0800

    Create openwrt-part3.sh

---
+++ openwrt-part3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
+git clone https://github.com/panther706/luci-app-adguardhome.git package/adguardhome
+#git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
+#git clone https://github.com/FUjr/QModem.git package/QModem
+git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support

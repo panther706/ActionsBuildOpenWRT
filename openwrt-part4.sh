34d8d7ecd831ce9f640d6bb54fdfd06f8b7b3d5a
panther706 <43943311+panther706@users.noreply.github.com>
2025-12-10 12:57:05 +0800

    Update openwrt-part4.sh

--- openwrt-part4.sh
+++ openwrt-part4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/10.0.0.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/10.0.0.1/192.168.10.1/g' package/base-files/files/bin/config_generate

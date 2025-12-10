6b593e59f8833c0403668a7b0a491f6326bc6c9c
panther706 <43943311+panther706@users.noreply.github.com>
2025-12-10 16:52:11 +0800

    Update openwrt-part3.sh

--- openwrt-part3.sh
+++ openwrt-part3.sh
@@ -7,5 +7,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 git clone https://github.com/panther706/luci-app-adguardhome.git package/adguardhome
 #git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
-git clone https://github.com/FUjr/QModem.git package/QModem
-#git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
+#git clone https://github.com/FUjr/QModem.git package/QModem
+git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support

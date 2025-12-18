42ec463009eb2d59939a8aae3defd0d82de545cf
panther706 <43943311+panther706@users.noreply.github.com>
2025-12-18 08:00:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,8 +7,9 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
 #git clone https://github.com/panther706/luci-app-adguardhome.git package/adguardhome
+
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-
-
-
-

86cd2905f2cc217318c8cb71e5f13f56f0ee18ca
panther706 <43943311+panther706@users.noreply.github.com>
2025-12-18 08:05:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,3 +3,7 @@
 # Modify default IP
 sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang

f990268eaf5e067d211b22824ad59a8ad00d2a78
panther706 <43943311+panther706@users.noreply.github.com>
2025-12-19 17:45:13 +0800

    Update q-part1.sh

--- q-part1.sh
+++ q-part1.sh
@@ -5,7 +5,8 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
-git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
-git clone https://github.com/FUjr/QModem.git package/QModem
 #git clone https://github.com/panther706/luci-app-adguardhome.git package/adguardhome
 #git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
+git clone https://github.com/FUjr/QModem.git package/QModem

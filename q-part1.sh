51cd4b2321886a11db06d3a6abba57d8746b9bfa
panther706 <43943311+panther706@users.noreply.github.com>
2026-01-02 07:45:35 +0800

    Update q-part1.sh

--- q-part1.sh
+++ q-part1.sh
@@ -4,8 +4,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+#sed -i "/helloworld/d" "feeds.conf.default"
+#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
 git clone https://github.com/FUjr/QModem.git package/QModem

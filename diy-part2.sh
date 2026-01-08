fcfe628835bba118130e590cd409eaea04bee06b
panther706 <43943311+panther706@users.noreply.github.com>
2026-01-08 19:55:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,3 +4,4 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+rm -rf feeds/luci/applications/luci-app-ssr-plus

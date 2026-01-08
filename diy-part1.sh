b19c988b30e7c33883697992a20c77b2d20d9d54
panther706 <43943311+panther706@users.noreply.github.com>
2026-01-09 07:47:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,8 +4,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i "/helloworld/d" "feeds.conf.default"
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
-#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+#sed -i "/helloworld/d" "feeds.conf.default"
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 #git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/adguardhome
 #git clone https://github.com/FUjr/QModem.git package/QModem

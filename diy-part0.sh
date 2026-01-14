96b7ed4965596f2552df0d4bdf0240960df11da3
panther706 <43943311+panther706@users.noreply.github.com>
2026-01-14 13:26:42 +0800

    Delete diy-part0.sh

--- diy-part0.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#git clone -b main --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-git clone -b master --depth=1 https://github.com/fw876/helloworld.git package/helloworld

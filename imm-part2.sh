2e94611fb66aeadd8f9744d768426248152aae7d
panther706 <43943311+panther706@users.noreply.github.com>
2026-01-16 19:11:14 +0800

    Update imm-part2.sh

--- imm-part2.sh
+++ imm-part2.sh
@@ -5,5 +5,5 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
-rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+#rm -rf feeds/packages/net/v2ray-geodata
+#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
